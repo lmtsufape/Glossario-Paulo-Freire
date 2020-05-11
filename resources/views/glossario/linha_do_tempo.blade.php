6b4301a03a70eb1c0907149f373a7f8a648d5805
carlos <carlos@debian.carlos>
2020-05-11 16:53:48 -0300

    Tirando links leia mais da view linha_do_tempo

--- resources/views/glossario/linha_do_tempo.blade.php
+++ resources/views/glossario/linha_do_tempo.blade.php
@@ -23,7 +23,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito">PAULO FREIRE <span class="titulo_vermelho_negrito">NASCEU</span> EM SETEMBRO DE 1921 NO RECIFE</h3>
                     <p class="direito paragrafo">
-                        Paulo Reglus Neves Freire nasceu em 19 de setembro de 1921 no Recife, capital do estado  brasileiro de Pernambuco. </br><a href="#">leia mais</a>
+                        Paulo Reglus Neves Freire nasceu em 19 de setembro de 1921 no Recife, capital do estado  brasileiro de Pernambuco. </br>
                     </p>
                 </div>
                     <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -84,7 +84,7 @@
                     <div class="row box_ano"></div> 
                     <h3 class="titulo_preto_negrito"><span class="titulo_vermelho_negrito">DEPRESSÃO</br></span> DE 1929</h3>
                     <p class="paragrafo">
-                        Sua família fazia parte da classe média, mas Paulo Freire vivenciou a pobreza e a fome na infância durante a depressão de 1929, uma experiência que o levaria a se preocupar com os mais pobres e o ajudaria a construir seu revolucionário método de alfabetização. </br><a href="#">leia mais</a>
+                        Sua família fazia parte da classe média, mas Paulo Freire vivenciou a pobreza e a fome na infância durante a depressão de 1929, uma experiência que o levaria a se preocupar com os mais pobres e o ajudaria a construir seu revolucionário método de alfabetização. </br>
                     </p>
                 </div>
             </div>
@@ -105,7 +105,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito">CURSA <span class="titulo_vermelho_negrito">DIREITO</span> NA UNIVERSIDADE DO RECIFE</h3>
                     <p class="direito paragrafo">
-                        Paulo Freire ingressa na Faculdade de Direito do Recife. Naquela época, o curso de direito era a única alternativa na área de ciências humanas. Nesse período, conheceu a professora primária Elza Maia Costa Oliveira... </br><a href="#">leia mais</a>
+                        Paulo Freire ingressa na Faculdade de Direito do Recife. Naquela época, o curso de direito era a única alternativa na área de ciências humanas. Nesse período, conheceu a professora primária Elza Maia Costa Oliveira... </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -166,7 +166,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito ">EM 1944 UNI-SE EM <span class="titulo_vermelho_negrito">MATRIMÔNIO</span></h3>
                     <p class="paragrafo">
-                        Paulo Freire ingressa na Faculdade de Direito do Recife. Naquela época, o curso de direito era a única alternativa na área de ciências humanas. Nesse período, conheceu a professora primária Elza Maia Costa Oliveira... </br><a href="#">leia mais</a>
+                        Paulo Freire ingressa na Faculdade de Direito do Recife. Naquela época, o curso de direito era a única alternativa na área de ciências humanas. Nesse período, conheceu a professora primária Elza Maia Costa Oliveira... </br>
                     </p>
                 </div>
             </div>
@@ -187,7 +187,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito">INDICADO AO CARGO DE <span class="titulo_vermelho_negrito">DIRETOR DO DEPARTAMENTO DE EDUCAÇÃO E CULTURA DO SERVIÇO SOCIAL</span> NO ESTADO DE PERNAMBUCO</h3>
                     <p class="direito paragrafo">
-                        Paulo Freire assume o cargo de Diretor do Departamento de Educação e Cultura do Serviço Social, onde travou contato com a questão da educação de adultos/trabalhadores e percebeu a necessidade de executar um trabalho direcionado à alfabetização. </br><a href="#">leia mais</a>
+                        Paulo Freire assume o cargo de Diretor do Departamento de Educação e Cultura do Serviço Social, onde travou contato com a questão da educação de adultos/trabalhadores e percebeu a necessidade de executar um trabalho direcionado à alfabetização. </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -248,7 +248,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito ">CONSTRUÇÃO DO </br><span class="titulo_vermelho_negrito">MÉTODO PAULO FREIRE</span></h3>
                     <p class="paragrafo">
-                        Em 1961 tornou-se diretor do Departamento de Extensões Culturais da Universidade do Recife e, no mesmo ano, realizou junto com sua equipe as primeiras experiências de alfabetização popular que levariam à constituição do Método Paulo Freire. </br><a href="#">leia mais</a>
+                        Em 1961 tornou-se diretor do Departamento de Extensões Culturais da Universidade do Recife e, no mesmo ano, realizou junto com sua equipe as primeiras experiências de alfabetização popular que levariam à constituição do Método Paulo Freire. </br>
                     </p>
                 </div>
             </div>
@@ -269,7 +269,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito">PRIMEIRAS <span class="titulo_vermelho_negrito">EXPERIÊNCIAS</span></h3>
                     <p class="direito paragrafo">
-                        Seu equipe foi responsável pela alfabetização de 300 cortadores de cana em apenas 45 dias. </br><a href="#">leia mais</a>
+                        Seu equipe foi responsável pela alfabetização de 300 cortadores de cana em apenas 45 dias. </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -330,7 +330,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito "><span class="titulo_vermelho_negrito">GOLPE</br></span> MILITAR</h3>
                     <p class="paragrafo">
-                        Em 1964, meses depois de iniciada a implantação do Plano, o golpe militar extinguiu esse esforço. Freire foi encarcerado como traidor por 70 dias. </br><a href="#">leia mais</a>
+                        Em 1964, meses depois de iniciada a implantação do Plano, o golpe militar extinguiu esse esforço. Freire foi encarcerado como traidor por 70 dias. </br>
                     </p>
                 </div>
             </div>
@@ -351,7 +351,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito"><span class="titulo_vermelho_negrito">EXÍLIO</br></span> NO CHILE</h3>
                     <p class="direito paragrafo">
-                        Durante o exílio, Paulo Freire passou pela Bolívia e trabalhou no Chile por cinco anos para o Movimento de Reforma Agrária da Democracia Cristã e para a Organização das Nações Unidas para a Agricultura e a Alimentação. </br><a href="#">leia mais</a>
+                        Durante o exílio, Paulo Freire passou pela Bolívia e trabalhou no Chile por cinco anos para o Movimento de Reforma Agrária da Democracia Cristã e para a Organização das Nações Unidas para a Agricultura e a Alimentação. </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -412,7 +412,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito ">CONCLUSÃO DO LIVRO <span class="titulo_vermelho_negrito">PEDAGOGIA DO OPRIMIDO</span></h3>
                     <p class="paragrafo">
-                        No ano anterior, ele havia concluído a redação de seu mais famoso livro, Pedagogia do Oprimido, que foi publicado em várias línguas como o espanhol, o inglês (em 1970) e até o hebraico (em 1981). </br><a href="#">leia mais</a>
+                        No ano anterior, ele havia concluído a redação de seu mais famoso livro, Pedagogia do Oprimido, que foi publicado em várias línguas como o espanhol, o inglês (em 1970) e até o hebraico (em 1981). </br>
                     </p>
                 </div>
             </div>
@@ -433,7 +433,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito">PROFESSOR VISITANTE DA <span class="titulo_vermelho_negrito">UNIVERSIDADE DE HARVARD</span></h3>
                     <p class="direito paragrafo">
-                        Freire foi convidado para ser professor visitante da Universidade Harvard em 1969. Nesse período, conheceu a professora primária Elza Maia Costa Oliveira... </br><a href="#">leia mais</a>
+                        Freire foi convidado para ser professor visitante da Universidade Harvard em 1969. Nesse período, conheceu a professora primária Elza Maia Costa Oliveira... </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -494,7 +494,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito "><span class="titulo_vermelho_negrito">RETORNO</br></span> AO BRASIL</h3>
                     <p class="paragrafo">
-                        Com a Anistia em 1979 Freire pôde retornar ao Brasil, mas só o fez em 1980. </br><a href="#">leia mais</a>
+                        Com a Anistia em 1979 Freire pôde retornar ao Brasil, mas só o fez em 1980. </br>
                     </p>
                 </div>
             </div>
@@ -515,7 +515,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito"><span class="titulo_vermelho_negrito">PRESIDENTE</span> DA 1º DIRETORIA EXECUTIVA DA FUNDAÇÃO WILSON PINHEIRO</h3>
                     <p class="direito paragrafo">
-                        Integrou o Partido dos Trabalhadores, tendo sido Presidente da 1ª Diretoria Executiva da Fundação Wilson Pinheiro, fundação de apoio partidária instituída pelo PT em 1981 (antecessora da Fundação Perseu Abramo). </br><a href="#">leia mais</a>
+                        Integrou o Partido dos Trabalhadores, tendo sido Presidente da 1ª Diretoria Executiva da Fundação Wilson Pinheiro, fundação de apoio partidária instituída pelo PT em 1981 (antecessora da Fundação Perseu Abramo). </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -576,7 +576,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito "><span class="titulo_vermelho_negrito">SECRETÁRIO DE EDUCAÇÃO</span> DA PREFEITURA DE SÃO PAULO</h3>
                     <p class="paragrafo">
-                        Freire foi nomeado secretário de Educação da cidade de São Paulo. Exerceu esse cargo de 1989 a 1991. </br><a href="#">leia mais</a>
+                        Freire foi nomeado secretário de Educação da cidade de São Paulo. Exerceu esse cargo de 1989 a 1991. </br>
                     </p>
                 </div>
             </div>
@@ -597,7 +597,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito direito"><span class="titulo_vermelho_negrito">FUNDAÇÃO</span> PAULO FREIRE</h3>
                     <p class="direito paragrafo">
-                        Em 1991, o Instituto Paulo Freire foi criado em São Paulo para ampliar e elaborar as suas teorias da educação popular. O instituto já tem projetos em muitos países e está sediada na Escola de Educação e Estudos de Informação da Universidade da Califórnia em Los Angeles (UCLA), onde arquivos de Freire são mantidos. </br><a href="#">leia mais</a>
+                        Em 1991, o Instituto Paulo Freire foi criado em São Paulo para ampliar e elaborar as suas teorias da educação popular. O instituto já tem projetos em muitos países e está sediada na Escola de Educação e Estudos de Informação da Universidade da Califórnia em Los Angeles (UCLA), onde arquivos de Freire são mantidos. </br>
                     </p>
                 </div>
                 <div class="col-md-2 coluna_vermelha texto_italico_central">
@@ -658,7 +658,7 @@
                     <div class="row box_ano"></div>
                     <h3 class="titulo_preto_negrito ">PAULO FREIRE <span class="titulo_vermelho_negrito">MORRE</span> AOS 70 ANOS</h3>
                     <p class="paragrafo">
-                        Freire morreu de um ataque cardíaco em 2 de maio de 1997, às 6h53, no Hospital Albert Einstein, em São Paulo. </br><a href="#">leia mais</a>
+                        Freire morreu de um ataque cardíaco em 2 de maio de 1997, às 6h53, no Hospital Albert Einstein, em São Paulo. </br>
                     </p>
                 </div>
             </div>
